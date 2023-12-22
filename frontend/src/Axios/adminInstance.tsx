29364da3ec020cc69a36e622d384a63c35ea5f4a
SahilAslam <sahilaslam14@gmail.com>
2023-12-22 20:14:13 +0530

    last commit

--- frontend/src/Axios/adminInstance.tsx
+++ frontend/src/Axios/adminInstance.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 const adminInstance = axios.create({
     // baseURL: "http://localhost:5000/admin",
     baseURL: "https://wandeo.website",
+    
 });
 
 adminInstance.interceptors.request.use(
